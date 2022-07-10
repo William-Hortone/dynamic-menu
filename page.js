46848c870f2f1214c7781497ba16b16b479b8a56
William Hortone <willhortone@gmail.com>
2022-07-10 13:59:08 +0800

    Display Dynamic items using js

--- page.js
+++ page.js
@@ -9,24 +9,25 @@ const menu = [
   },
   {
     id: 2,
-    title: " Tomato Burger King",
-    category: "burger",
-    price: "20,99",
-    img: "./images/4.jpg",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea necessitatibus ducimus labore dolor, eius tempore",
+    title: "Cocktail",
+    category: "drink",
+    price: "9,99",
+    img: "./images/12.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
   },
+
   {
     id: 3,
-    title: "Chicken Buttermilk",
-    category: "burger",
-    price: "15,99",
-    img: "./images/2.jpg",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea necessitatibus ducimus labore dolor, eius tempore",
+    title: "Spaghetti",
+    category: "dinner",
+    price: "22,99",
+    img: "./images/7.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
   },
 
   {
     id: 4,
-    title: "Pancakes Buttermilk",
+    title: "Simple Burger",
     category: "burger",
     price: "18,99",
     img: "./images/6.jpg",
@@ -34,38 +35,6 @@ const menu = [
   },
   {
     id: 5,
-    title: "Spaguetti",
-    category: "dinner",
-    price: "22,99",
-    img: "./images/7.jpg",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
-  },
-  {
-    id: 6,
-    title: "Spaguetti",
-    category: "dinner",
-    price: "25,99",
-    img: "./images/8.jpg",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
-  },
-  {
-    id: 7,
-    title: "Spaguetti",
-    category: "dinner",
-    price: "30,99",
-    img: "./images/9.webp",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
-  },
-  {
-    id: 8,
-    title: "Spaguetti",
-    category: "dinner",
-    price: "32,99",
-    img: "./images/10.jpg",
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
-  },
-  {
-    id: 9,
     title: "Orange Cocktail",
     category: "drink",
     price: "12,99",
@@ -73,12 +42,45 @@ const menu = [
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
   },
   {
-    id: 10,
-    title: "Cocktail",
-    category: "drink",
-    price: "9,99",
-    img: "./images/12.jpg",
+    id: 6,
+    title: "Chicken Salade",
+    category: "dinner",
+    price: "32,99",
+    img: "./images/10.jpg",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
+  },
+
+  {
+    id: 7,
+    title: "Pancakes Buttermilk",
+    category: "dinner",
+    price: "30,99",
+    img: "./images/9.webp",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
+  },
+  {
+    id: 8,
+    title: "Chicken Buttermilk",
+    category: "burger",
+    price: "15,99",
+    img: "./images/2.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea necessitatibus ducimus labore dolor, eius tempore",
+  },
+  {
+    id: 9,
+    title: "Chicken Buttermilk",
+    category: "dinner",
+    price: "25,99",
+    img: "./images/8.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea nectempore",
+  },
+  {
+    id: 10,
+    title: " Tomato Burger King",
+    category: "burger",
+    price: "20,99",
+    img: "./images/4.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis animi aliquid maxime minima nesciunt praesentium voluptas quaerat, quis ea necessitatibus ducimus labore dolor, eius tempore",
   },
   {
     id: 11,
@@ -90,76 +92,49 @@ const menu = [
   },
 ];
 
-const container = document.querySelector(".container");
+let container = document.querySelector(".container");
 const buttons = document.querySelectorAll(".button");
 
 window.addEventListener("DOMContentLoaded", () => {
-  displayMenuItem(menu);
+  displayMenuItems(menu);
 });
-function displayMenuItem(menuItems) {
-  let displayMenu = menuItems.map((item) => {
- 
-    return `<article class="food_container">
 
-        <div class="image_box">
-            <img src=${item.img} alt="#" id="food_img">
-        </div>
-         
-        <div class="text_container">
-            <div class="title_price">
-                <h2 id="food_title"> ${item.title}</h2>
-                <p>$<span id="price">${item.price}</span></p>
-            </div>
+function displayMenuItems(itemsMenu) {
+  let displayMenu = itemsMenu.map((items) => {
+    return ` <article class="food_container">
+    <div class="image_box">
+      <img src="${items.img}" alt="#" id="food_img" />
+    </div>
 
-            <div>
-                <p id="description">${item.text}</p>
-            </div>
-        </div>
-        
-    </article>`;
+    <div class="text_container">
+      <div class="title_price">
+        <h2 id="food_title">${items.title}</h2>
+        <p>$<span id="price">${items.price}</span></p>
+      </div>
+
+      <div>
+        <p id="description">${items.text}</p>
+      </div>
+    </div>
+  </article>`;
   });
   displayMenu = displayMenu.join("");
   container.innerHTML = displayMenu;
 }
+
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const category = e.currentTarget.dataset.id;
+    let category = e.currentTarget.dataset.id;
 
-    const menuCategory = menu.filter(function (menuItem) {
-      if (menuItem.category === category) {
-        return menuItem;
+    let displayCategory = menu.filter((item) => {
+      if (item.category == category) {
+        return item;
       }
     });
-
     if (category === "all") {
-      displayMenuItem(menu);
+      displayMenuItems(menu);
     } else {
-      displayMenuItem(menuCategory);
+      displayMenuItems(displayCategory);
     }
   });
 });
-
-function displayMenuItem(menuItems) {
-  let displayMenu = menuItems.map((item) => {
-    return `<article class="food_container">
-
-        <div class="image_box">
-            <img src=${item.img} alt="#" id="food_img">
-        </div>
-         
-        <div class="text_container">
-            <div class="title_price">
-                <h2 id="food_title"> ${item.title}</h2>
-                <p>$<span id="price">${item.price}</span></p>
-            </div>
-
-            <div>
-                <p id="description">${item.text}</p>
-            </div>
-        </div>
-        
-    </article>`;
-  });
-  displayMenu = displayMenu.join("");
-  container.innerHTML = displayMenu;
-}
